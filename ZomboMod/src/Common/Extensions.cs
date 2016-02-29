e6718faf1edbdca34af0ca4ef9b692a1e0a86dc6
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-02-29 04:20:20 -0300

    Update

---
+++ ZomboMod/src/Common/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZomboMod.Common
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>( this T[] array, Action<T> act )
+        {
+            foreach ( var obj in array ) act( obj );
+        }
+
+        public static void ForEach<T>( this IEnumerable<T> enumerable, Action<T> act )
+        {
+            foreach ( var obj in enumerable ) act( obj );
+        }
+
+        public static V GetOrDefault<K, V>( this Dictionary<K, V> dict, K key, V def )
+        {
+            V value;
+            return dict.TryGetValue( key, out value ) ? value : def;
+        }
+    }
+}
---
+++ ZomboMod/src/Permission/Internal/PermissionProvider.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ZomboMod.Common;
+using ZomboMod.Entity;
+
+namespace ZomboMod.Permission.Internal
+{
+    internal class PermissionProvider : IPermissionProvider
+    {
+        internal PermissionStorage Storage;
+
+        internal PermissionProvider()
+        {
+            Storage = new PermissionStorage();
+        }
+
+        public List<string> GetPermissions( UPlayer player )
+        {
+            return GetPermissions( player.SteamProfile.SteamID.m_SteamID );
+        }
+
+        public List<string> GetPermissions( ulong playerId )
+        {
+            var ret = new List<string>();
+            PermissionPlayer permPlayer;
+            var groupFound = false;
+
+            if ( Storage.Players.TryGetValue( playerId, out permPlayer ) )
+            {
+                ret.AddRange( permPlayer.Permissions );
+
+                permPlayer.Groups.ForEach( g => {
+                    ret.AddRange( g.Permissions );
+                    groupFound = true;
+                } );
+            }
+
+            if ( !groupFound )
+            {
+                Storage.Groups.Values
+                        .Where( g => g.Players.Contains( playerId ) )
+                        .ForEach( g => ret.AddRange( g.Permissions ));
+            }
+            
+            return ret;
+        }
+
+        public bool HasPermission( UPlayer player, string permission )
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasPermission( ulong playerId, string permission )
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PermissionGroup GetGroup( string name )
+        {
+            return Storage.Groups.GetOrDefault( name, null );
+        }
+
+        public PermissionPlayer GetPlayer( ulong playerId )
+        {
+            return Storage.Players.GetOrDefault( playerId, null );
+        }
+
+        public void Load()
+        {
+            Storage.Load();
+        }
+
+        public void Save()
+        {
+            Storage.Save();
+        }
+    }
+}
